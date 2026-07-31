fbfa7250194de1704ea82200c652860c5bad23a5
vaugustin250 <vaugustin250@gmail.com>
2026-07-31 16:48:08 +0530

    fix: pwa splash screen dark mode inversion and masking

--- vite.config.js
+++ vite.config.js
@@ -23,13 +23,13 @@ export default defineConfig({
             src: 'vbills-icon-white-192.png',
             sizes: '192x192',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
           },
           {
             src: 'vbills-icon-white-512.png',
             sizes: '512x512',
             type: 'image/png',
-            purpose: 'any maskable'
+            purpose: 'any'
           }
         ],
         categories: ['business', 'productivity'],
